4c767a61abde3acf5b76d33ed94276df0673fd5e
Nico Fuenzalida <nico01f@gmail.com>
2015-12-27 06:29:15 -0300

    Validacion agregada a when_activate

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -4,17 +4,10 @@ class Reminder < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  validate :not_empty_when_activate
+  validate :when_activate
 
-=begin
-  def greater_than_now
-    if when_activate <= DateTime.now
-      errors.add :base, 'Cuando activar debe ser mayor a la fecha actual.'
-    end
-  end
-=end
 
-  def not_empty_when_activate
+  def when_activate
     if when_activate.nil? or when_activate <= DateTime.now
       errors.add :base, 'Cuando activar no puede quedar en blanco o la fecha es menor a la actual.'
     end
