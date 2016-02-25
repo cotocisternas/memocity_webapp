6d4eb4c8e1b0518ff03b2c47ae4abf2841094c11
Nico Fuenzalida <nico01f@gmail.com>
2016-02-25 19:16:29 -0300

    memocity reminder finished

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,5 +1,6 @@
 class RemindersController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy, :edit, :update]
+  before_action :correct_user, only: :destroy
 
   def create
     @reminder = current_user.reminders.build(reminder_params)
@@ -13,7 +14,9 @@ class RemindersController < ApplicationController
   end
 
   def destroy
-    # code
+    @reminder.destroy
+    flash[:success] = "Recordatorio eliminado"
+    redirect_to request.referrer || root_url
   end
 
   def edit
@@ -28,5 +31,10 @@ class RemindersController < ApplicationController
 
   def reminder_params
     params.require(:reminder).permit(:content, :when_activate)
+  end
+
+  def correct_user
+    @reminder = current_user.reminders.find_by(id: params[:id])
+    redirect_to root_url if @reminder.nil?
   end
 end
--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -9,7 +9,7 @@ class Reminder < ActiveRecord::Base
   def eval_when_activate
     if when_activate.nil?
       errors.add :base, 'Cuando activar no puede quedar en blanco.'
-    elsif when_activate >= DateTime.now
+    elsif when_activate <= DateTime.now
       errors.add :base, 'Fecha/Hora debe ser mayor a la actual.'
     end
   end
