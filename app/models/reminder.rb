77217d687a8cef6c4985615ff0382c2b9226c154
Nico Fuenzalida <nico01f@gmail.com>
2016-02-26 01:42:10 -0300

    fix validation on datetime

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -3,14 +3,30 @@ class Reminder < ActiveRecord::Base
   default_scope -> { order(created_at: :desc) }
 
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 }
+  #validates :content, presence: true, length: { maximum: 140 }
+  validate :contentReminder
   validate :eval_when_activate
+
+
+
+  private
+
+  def contentReminder
+    if content.empty?
+      errors.add :base, 'Contenido no puede quedar en blanco'
+    elsif content.length >= 140
+      errors.add :base, 'El contenido no puede superar los 140 caracteres'
+    end
+  end
 
   def eval_when_activate
     if when_activate.nil?
-      errors.add :base, 'Cuando activar no puede quedar en blanco.'
-    elsif when_activate <= DateTime.now
-      errors.add :base, 'Fecha/Hora debe ser mayor a la actual.'
+      errors.add :base, 'Cuando activar no puede quedar en blanco'
+    end
+    if when_activate <= Time.zone.now
+      errors.add :base, 'Fecha/Hora debe ser mayor a la actual'
     end
   end
+
+
 end
