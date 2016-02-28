820829b31125e6c0013e3ea887fe7fa2fc6a46f3
Nico Fuenzalida <nico01f@gmail.com>
2016-02-28 03:51:29 -0300

    improve validation of when_activate

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -3,27 +3,27 @@ class Reminder < ActiveRecord::Base
   default_scope -> { order(created_at: :desc) }
 
   validates :user_id, presence: true
-  #validates :content, presence: true, length: { maximum: 140 }
-  validate :contentReminder
+  #validates :content, presence: true, length: { maximum: 120 }
+  validate :eval_content_reminder
   validate :eval_when_activate
 
 
 
   private
 
-  def contentReminder
+  def eval_content_reminder
     if content.empty?
       errors.add :base, 'Contenido no puede quedar en blanco'
-    elsif content.length >= 140
-      errors.add :base, 'El contenido no puede superar los 140 caracteres'
+    elsif content.length > 120
+      errors.add :base, 'El contenido no puede superar los 120 caracteres'
     end
   end
 
   def eval_when_activate
-    if when_activate.nil?
-      errors.add :base, 'Cuando activar no puede quedar en blanco'
+    if when_activate.to_s.empty?
+      errors.add :base, 'Fecha/Hora no fue ingresada'
     end
-    if when_activate <= Time.zone.now
+    if when_activate.to_s == Time.zone.now.to_s
       errors.add :base, 'Fecha/Hora debe ser mayor a la actual'
     end
   end
